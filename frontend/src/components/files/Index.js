4dbcbbda0cd534b5f0a6a6a37ad890d9c18e438f
Dmitry Boiko <dmitrygboyko@gmail.com>
2020-01-29 17:38:44 +0200

    Update calculateTotalAmount function

--- frontend/src/components/files/Index.js
+++ frontend/src/components/files/Index.js
@@ -9,7 +9,7 @@ import { authenticate, clearAccessToken, selectFile, uploadFile } from '../../ac
 
 function Main(props) {
     const calculateTotalAmount = (files) => {
-        if (files && files.length == 0) {
+        if (!files || files.length == 0) {
             return 0;
         }
 
